ea0a9bdbb95b8ee71d6660ee85ddc481928942cf
bear1030 <bear1030@hotmail.com>
2020-11-24 17:53:41 +0800

    Return upload file url instead of file name.

--- android/src/main/java/com/azureblobstorage/FileManager.java
+++ android/src/main/java/com/azureblobstorage/FileManager.java
@@ -24,12 +24,13 @@ import com.microsoft.azure.storage.blob.CloudBlobClient;
 import com.microsoft.azure.storage.blob.CloudBlobContainer;
 import com.microsoft.azure.storage.blob.CloudBlockBlob;
 import com.microsoft.azure.storage.blob.ListBlobItem;
+import com.microsoft.azure.storage.blob.SharedAccessBlobPolicy;
+import com.microsoft.azure.storage.blob.SharedAccessBlobPermissions;
 
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.security.SecureRandom;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class FileManager {
     /*
@@ -73,12 +74,21 @@ public class FileManager {
         BlobContainerPermissions permissions = container.downloadPermissions();
         container.uploadPermissions(permissions);
 
-        String imageName = fileName; 
-        CloudBlockBlob imageBlob = container.getBlockBlobReference(imageName);
+        SharedAccessBlobPolicy sasPolicy = new SharedAccessBlobPolicy();
+        GregorianCalendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+        calendar.add(Calendar.HOUR, 10);
+        sasPolicy.setSharedAccessExpiryTime(calendar.getTime());
+
+        sasPolicy.setPermissions(EnumSet.of(SharedAccessBlobPermissions.READ,
+                SharedAccessBlobPermissions.WRITE,
+                SharedAccessBlobPermissions.LIST));
+
+
+        CloudBlockBlob imageBlob = container.getBlockBlobReference(fileName);
         imageBlob.getProperties().setContentType(contentType);
         imageBlob.upload(image, imageLength);
-
-        return imageName;
+        String sas = imageBlob.generateSharedAccessSignature(sasPolicy, null);
+        return imageBlob.getUri().toString() + "?" + sas;
 
     }
 
